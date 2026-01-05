4ff86a02c069bbdc3fd6b7a4d218bf44bc808546
Bhavesh <bhaveshanjana58@gmail.com>
2026-01-05 17:24:50 +0530

    feat: working on todo's

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,72 @@
 import { createHomeStyles } from "@/assets/styles/home.style";
 import useTheme from "@/hooks/useTheme";
-import { StatusBar, Text, TouchableOpacity, View } from "react-native";
+import { Alert, FlatList, StatusBar, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
 import Header from "@/components/Header";
+import TodoInput from "@/components/TodoInput";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import { Ionicons } from "@expo/vector-icons";
+import EmptyState from "@/components/EmptyState";
+
+type Todo = Doc<"todos">
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
   const HomeStyles = createHomeStyles(colors);
+  const todos = useQuery(api.todos.getTodos)
+  const toggleTodo = useMutation(api.todos.toggleTodo)
+  const isLoading = todos === undefined
+  if (isLoading) return <LoadingSpinner />
+
+  const handleToggleItem = async (id: Id<"todos">) => {
+    try {
+
+      const ToggleItem = await toggleTodo({ id })
+    } catch (error) {
+      Alert.alert("Error", "Faild to update todo")
+    }
+  }
+  const renderTodoItem = ({ item }: { item: Todo }) => {
+    return (
+      <View style={HomeStyles.todoItemWrapper}>
+        <LinearGradient colors={colors.gradients.surface} style={HomeStyles.todoItem} start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }}>
+          <TouchableOpacity style={HomeStyles.checkbox} activeOpacity={0.7} onPress={() => handleToggleItem(item._id)}>
+            <LinearGradient colors={item.isCompleted ? colors.gradients.success : colors.gradients.muted} style={[HomeStyles.checkboxInner, { borderColor: item.isCompleted ? "transparent" : colors.border },]}>
+              {
+                item.isCompleted && <Ionicons name="checkmark" size={18} color="#fff" />
+              }
+            </LinearGradient>
+          </TouchableOpacity>
+          <View style={HomeStyles.todoTextContainer}>
+            <Text style={[HomeStyles.todoText, item.isCompleted && {
+              textDecorationLine: "line-through", color: colors.textMuted, opacity: 0.6
+            },
+            ]}>
+              {item.text}
+            </Text>
+            <View style={HomeStyles.todoActions}>
+              <TouchableOpacity onPress={() => { }} activeOpacity={0.8}>
+                <LinearGradient colors={colors.gradients.warning} style=
+                  {HomeStyles.actionButton}>
+                  <Ionicons name="pencil" size={14} color="#fff" />
+                </LinearGradient>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => { }} activeOpacity={0.8}>
+                <LinearGradient colors={colors.gradients.danger} style=
+                  {HomeStyles.actionButton}>
+                  <Ionicons name="trash" size={14} color="#fff" />
+                </LinearGradient>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </LinearGradient>
+      </View>
+    )
+  }
   return (
     <LinearGradient colors={colors.gradients.background} style={HomeStyles.container}>
       <StatusBar barStyle={colors.statusBarStyle} />
@@ -16,6 +75,15 @@ export default function Index() {
           <Text className="">Switch mode</Text>
         </TouchableOpacity>
         <Header />
+        <TodoInput />
+        <FlatList
+          data={todos}
+          renderItem={renderTodoItem}
+          keyExtractor={(item) => item._id}
+          style={HomeStyles.todoList}
+          contentContainerStyle={HomeStyles.todoListContent}
+          ListEmptyComponent={<EmptyState />}
+        />
       </SafeAreaView>
     </LinearGradient>
   );
---
+++ components/TodoInput.tsx
@@ -0,0 +1,49 @@
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native'
+import React, { useState } from 'react'
+import useTheme from '@/hooks/useTheme'
+import { createHomeStyles } from '@/assets/styles/home.style'
+import { useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { LinearGradient } from 'expo-linear-gradient'
+import { Ionicons } from '@expo/vector-icons'
+
+const TodoInput = () => {
+    const { colors } = useTheme()
+    const homeStyle = createHomeStyles(colors);
+    const [newTodo, setNewTodo] = useState("");
+    const addTodo = useMutation(api.todos.addTodo);
+
+    const handleAddTodo = async () => {
+        if (newTodo.trim()) {
+            try {
+                await addTodo({ text: newTodo.trim() })
+                setNewTodo("")
+            } catch (error) {
+                Alert.alert("Error", "Faild to add todo")
+            }
+        }
+    };
+    return (
+        <View style={homeStyle.inputSection}>
+            <View style={homeStyle.inputWrapper}>
+                <TextInput
+                    style={homeStyle.input}
+                    placeholder="Enter todo"
+                    value={newTodo}
+                    onChangeText={setNewTodo}
+                    onSubmitEditing={handleAddTodo}
+                    placeholderTextColor={colors.textMuted}
+                />
+                <TouchableOpacity onPress={handleAddTodo} activeOpacity={0.8} disabled={!newTodo.trim()}>
+                    <LinearGradient colors={newTodo.trim() ? colors.gradients.primary : colors.gradients.muted}
+                        style={[homeStyle.addButton, !newTodo.trim() && homeStyle.addButtonDisabled]}
+                    >
+                        <Ionicons name='add' size={24} color={"ffffff"} />
+                    </LinearGradient>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+export default TodoInput
